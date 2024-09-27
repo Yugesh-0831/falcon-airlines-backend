14266aa77e354daadd6a956df3164a44214c7549
Yugesh-0831 <yugeshjhamb0831@gmail.com>
2024-09-27 21:40:50 +0530

    updated backend

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createUser, fetchUserById, updateUser } = require("../controller/user");
+const { createUser, fetchUserById, updateUser } = require("../controller/User");
 
 const router = express.Router();
 router.get("/:id", fetchUserById).patch("/:id", updateUser);
